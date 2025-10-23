f1bbb54bfe087fc42c055cb4784ae442c5f8e046
NguyenHoangKimYen <183996267+NguyenHoangKimYen@users.noreply.github.com>
2025-10-23 09:29:20 +0700

    refactor cart model

--- backend/src/models/cart.model.js
+++ backend/src/models/cart.model.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const CartSchema = new mongoose.Schema({
-  // Trường _id (PK) tự động được Mongoose/MongoDB tạo ra
   
-  // Khóa ngoại: Chủ sở hữu giỏ hàng
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Tham chiếu đến Model 'User'
+    ref: 'User',
     required: false, // Cho phép null/undefined (khách vãng lai)
     default: null,
     // Nên tạo index độc nhất nếu userId tồn tại để 1 user chỉ có 1 cart
