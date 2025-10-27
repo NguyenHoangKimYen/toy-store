b4b7c3c9c5f329be1d6c1700633e5f2e84424cd3
NguyenHoangKimYen <183996267+NguyenHoangKimYen@users.noreply.github.com>
2025-10-27 09:32:38 +0700

    fix update product function and create product function with uploading images

--- backend/src/middlewares/upload.middleware.js
+++ backend/src/middlewares/upload.middleware.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ storage }).array('images', 5);
 
 module.exports = upload;
