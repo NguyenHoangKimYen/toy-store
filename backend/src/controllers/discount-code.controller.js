1d7b418c27aba041ad33ddbdd53e173dd7ced501
Vo Xuan Quang <vxq123@icloud.com>
2025-11-29 12:23:49 +0700

    Revert "feat: pass query params to discount code service"
    This reverts commit 0c32a7896e00a33a07a5e22bbde00bc17ac15675.

--- backend/src/controllers/discount-code.controller.js
+++ backend/src/controllers/discount-code.controller.js
@@ -45,7 +45,7 @@ module.exports = {
 
     async getAll(req, res) {
         try {
-            const list = await discountService.getAll(req.query);
+            const list = await discountService.getAll();
             return res.json({ success: true, data: list });
         } catch (err) {
             console.error(err);
